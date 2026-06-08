c95b67c230bb5dc05c409055b9cd1674e4c04aa8
Wazed Ali Imam <imamw7428@gmail.com>
2026-06-08 00:56:51 -0400

    .

--- src/main.c
+++ src/main.c
@@ -7,7 +7,7 @@
 #include "../inc/helper.h"
 #include "../inc/PCR1NeckAndLyndon.h"
 #include "../inc/PCR3NeckAndLyndon.h"
-#include "../inc/trails.h"
+#include "../inc/trials.h"
 #include "../inc/interface.h"
 
 int main(){
